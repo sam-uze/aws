78ee1e3fce1464bb4e8bcf404d9754bed1b1798a
Samuel Uzelino <uzelino.sam@gmail.com>
2025-05-05 14:25:11 +0200

    Refactor popup content for weather stations to include name and elevation

--- main.js
+++ main.js
@@ -51,12 +51,9 @@ async function loadStations(url) {
             });
         },
         onEachFeature: function (feature, layer) {
-            //console.log(feature.properties);
+            console.log(feature);
             layer.bindPopup(`
-                <img src="${feature.properties.THUMBNAIL}" alt="*">
-                <h4>${feature.properties.NAME}</h4>
-                <addess>${feature.properties.ADRESSE}</addess>
-                <a href="${feature.properties.WEITERE_INF}"target="Wien">Webseite</a>
+                <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
             `);
         }
     }).addTo(overlays.stations);
