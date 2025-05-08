00d9decabe778b8cba80fadfd18d89a1395e9450
Samuel Uzelino <uzelino.sam@gmail.com>
2025-05-08 18:28:26 +0200

    Add wind color management and display functionality

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ let map = L.map("map").setView([ibk.lat, ibk.lng], ibk.zoom);
 let overlays = {
     stations: L.featureGroup(),
     temperature: L.featureGroup().addTo(map),
+    wind: L.featureGroup(),
 }
 
 // Layer control
@@ -28,6 +29,7 @@ L.control.layers({
 }, {
     "Wetterstationen": overlays.stations,
     "Temperatur": overlays.temperature,
+    "Wind": overlays.wind,
 }).addTo(map);
 
 // Maßstab
@@ -69,6 +71,8 @@ async function loadStations(url) {
         }
     }).addTo(overlays.stations)
     showTemperature(jsondata);
+    showWind(jsondata);
+
 }
 
 
@@ -88,11 +92,32 @@ function showTemperature(jsondata) {
             return L.marker(latlng,{
                 icon: L.divIcon({
                     className: "aws-div-icon",
-                    html: `<span style="background-color:${color}"> ${feature.properties.LT}</span>`
-                }),
+                    html: `<span style="background-color:${color}"> ${feature.properties.LT.toFixed(1)}</span>`
+                })
             })
         },
     }).addTo(overlays.temperature);
+}
+
+
+function showWind(jsondata) {
+    L.geoJSON(jsondata, {
+        filter: function (feature) {
+            if (feature.properties.WG > 0 && feature.properties.WG < 150) {
+                return true;
+            }
+
+        },
+        pointToLayer: function (feature, latlng){
+            let color = getColor(feature.properties.WG, COLORS.wind);
+            return L.marker(latlng,{
+                icon: L.divIcon({
+                    className: "aws-div-icon-wind",
+                    html: `<span style="background-color:${color}"> ${feature.properties.WG.toFixed(1)}</span>`
+                })
+            })
+        },
+    }).addTo(overlays.wind);
 }
 
 console.log(COLORS);
@@ -104,5 +129,3 @@ function getColor(value, ramp) {
         
     }
 }
-let testColor = getColor(-5, COLORS.temperature);
-console.log("TestColor fuer temp -5", testColor);
