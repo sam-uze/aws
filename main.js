9ffb53182dbcb93d8ed0f86cdf2079f5825837c9
Samuel Uzelino <uzelino.sam@gmail.com>
2025-05-15 15:58:05 +0200

    Enhance wind direction display with rotation and icon

--- main.js
+++ main.js
@@ -177,7 +177,7 @@ function showDirect(jsondata) {
             return L.marker(latlng,{
                 icon: L.divIcon({
                     className: "aws-div-icon-wind",
-                    html: `<span style="background-color:${color}"> ${feature.properties.WR.toFixed(1)}°</span>`
+                    html: `<span style="transform:rotate(${feature.properties.WR}deg); background-color:${color}"> <i class="fa-solid fa-circle-arrow-down"></i></span>`,
                 })
             })
         },
